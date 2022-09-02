179d07287a6230ad5245e99a4e11e3fb25f226dd
omerfurkank <omerfurkankilic@yandex.com>
2022-09-02 23:01:38 +0300

    database connected

---
+++ src/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+
+
+        public BaseDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProgrammingLanguage>(a =>
+            {
+                a.ToTable("ProgrammingLanguages").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.Name).HasColumnName("Name").HasMaxLength(50).IsRequired();
+            });
+
+            ProgrammingLanguage[] programmingLanguageSeeds = { new(1, "C#"), new(2, "Java"), new(3, "Go") };
+            modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguageSeeds);
+
+
+        }
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,9 +1,16 @@
+using Core.CrossCuttingConcerns.Exceptions;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddApplicationServices();
+//builder.Services.AddSecurityServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+//builder.Services.AddInfrastructureServices();
+//builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,6 +22,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
